1b51d8bda82a37b713c96770ba029be093016c74
aguai <aguaithefreak@gmail.com>
2017-12-07 14:56:54 +0800

    reorgnize and switch to golang

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # wave-reader
-get wave file's metadata   
+dump wave file's metadata and signal data to study   
 
 以 https://github.com/cw1997/wave-resolver 为基础    
 
-使用C语言用于解析wav文件    
+使用 ~~C语言~~ Go 語言解析 wav文件    
 
 原始教程 https://zhuanlan.zhihu.com/p/27338283     
--- src/readwav.go
+++ src/readwav.go
@@ -41,7 +41,7 @@ func main() {
 	}
 	filename := os.Args[1]
 	f, _ := os.Open(filename)
-	Q := Wav{}
+	Q := new(Wav)
 	binary.Read(f, binary.BigEndian, &Q.WaveChuck.ChuckID)
 	binary.Read(f, binary.LittleEndian, &Q.WaveChuck.ChuckDataSize)
 	binary.Read(f, binary.BigEndian, &Q.WaveChuck.RiffType)
@@ -53,7 +53,6 @@ func main() {
 	binary.Read(f, binary.LittleEndian, &Q.FormatChuck.BytePerSec)
 	binary.Read(f, binary.LittleEndian, &Q.FormatChuck.BlockAlign)
 	binary.Read(f, binary.LittleEndian, &Q.FormatChuck.BitDepth)
-
 	binary.Read(f, binary.BigEndian, &Q.SoundDataChuck.ChuckID)
 	binary.Read(f, binary.LittleEndian, &Q.SoundDataChuck.SoundDataSize)
 	riff := ""
@@ -73,15 +72,24 @@ func main() {
 	for i := range Q.SoundDataChuck.ChuckID {
 		data += string(Q.SoundDataChuck.ChuckID[i])
 	}
-	fmt.Printf("RIFF:\t\t%s\n", riff)
-
-	fmt.Printf("WAVE:\t\t\t%s\n", wave)
-	fmt.Printf("WFMT:\t\t\t%s\n", wfmt)
-	fmt.Printf("DATA:\t\t\t%s\n", data)
-	fmt.Printf("WaveChuckDataSize:\t%d\n", Q.WaveChuck.ChuckDataSize)
-	fmt.Printf("SoundDataChuckDataSize:\t%d\n", Q.SoundDataChuck.SoundDataSize)
-	fmt.Printf("BitDepth:\t\t%d\n", Q.FormatChuck.BitDepth)
-	fmt.Printf("Sample Rate:\t%d\n", Q.FormatChuck.SampleRate)
+	fmt.Printf("RIFF:\t\t\t\t%s\n", riff)
+	fmt.Printf("WAVE:\t\t\t\t%s\n", wave)
+	fmt.Printf("WFMT:\t\t\t\t%s\n", wfmt)
+	fmt.Printf("DATA:\t\t\t\t%s\n", data)
+	fmt.Printf("WaveChuckDataSize:\t\t%d\n", Q.WaveChuck.ChuckDataSize)
+	fmt.Printf("SoundDataChuckDataSize:\t\t%d\n", Q.SoundDataChuck.SoundDataSize)
+	fmt.Printf("BitDepth:\t\t\t%d\n", Q.FormatChuck.BitDepth)
+	fmt.Printf("Sample Rate:\t\t\t%d\n", Q.FormatChuck.SampleRate)
+	fmt.Printf("WaveChuckDataSize:\t\t%d\n", Q.WaveChuck.ChuckDataSize)
+	fmt.Printf("FormatChuckDataSize:\t\t%d\n", Q.FormatChuck.ChuckDataSize)
+	fmt.Printf("FormatTag:\t\t\t%d\n", Q.FormatChuck.FormatTag)
+	fmt.Printf("NumOfChannels:\t\t\t%d\n", Q.FormatChuck.NumOfChannels)
+	fmt.Printf("SampleRate:\t\t\t%d\n", Q.FormatChuck.SampleRate)
+	fmt.Printf("BytePerSec:\t\t\t%d\n", Q.FormatChuck.BytePerSec)
+	fmt.Printf("BlockAlign:\t\t\t%d\n", Q.FormatChuck.BlockAlign)
+	fmt.Printf("BitDepth:\t\t\t%d\n", Q.FormatChuck.BitDepth)
+	fmt.Printf("SoundDataChuckSize:\t\t%d\n", Q.SoundDataChuck.SoundDataSize)
+	fmt.Printf("%d 個 sample\n", Q.SoundDataChuck.SoundDataSize/(uint32(Q.FormatChuck.NumOfChannels)*uint32(Q.FormatChuck.BitDepth/8)))
 
 	return
 }
